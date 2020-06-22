290b25b892c601c0e98c3415a03cabd1dac99619
Nicholas Parsons <parsons.nicholas1@gmail.com>
2020-06-22 15:09:51 -0400

    Added new templates and stylesheet for blog.

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,13 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published_all()
+    posts = Post.published.all()
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug-post,
+    post = get_object_or_404(Post, slug=post,
                                    status='published',
                                    publish__year=year,
                                    publish__month=month,
